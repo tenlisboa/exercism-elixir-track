7f2602282ec77f6842ad52c8a6b73e72be11e20a
Gabriel Lisboa <gabriellisboa.rx@gmail.com>
2024-05-16 14:15:14 -0300

    feat: using bitstrings

---
+++ paint-by-number/lib/paint_by_number.ex
@@ -0,0 +1,36 @@
+defmodule PaintByNumber do
+  def palette_bit_size(color_count, exponent \\ 0) do
+    case Integer.pow(2, exponent) do
+      n when n >= color_count -> exponent
+      _ -> palette_bit_size(color_count, exponent + 1)
+    end
+  end
+
+  def empty_picture(), do: <<>>
+
+  def test_picture(), do: <<0::2, 1::2, 2::2, 3::2>>
+
+  def prepend_pixel(picture, color_count, pixel_color_index) do
+    << pixel_color_index::size(palette_bit_size(color_count)), picture::bitstring >> 
+  end
+
+  def get_first_pixel(picture, color_count) do
+    size = palette_bit_size(color_count)
+
+    case picture do
+      << val::size(size), _rest::bitstring >> -> val
+      _ -> nil 
+    end
+  end
+
+  def drop_first_pixel(<<>>, _color_count), do: empty_picture()
+  def drop_first_pixel(picture, color_count) do
+    size = palette_bit_size(color_count)
+
+    << _::size(size), rest::bitstring >> = picture
+
+    rest
+  end
+
+  def concat_pictures(picture1, picture2), do: <<picture1::bitstring, picture2::bitstring>>
+end
